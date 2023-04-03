1392c91b3e8e08f6acd62ba034f965a262e03437
Sushil Ale <msucil@Sushils-MacBook-Pro.local>
2023-04-03 13:43:40 +0800

    Add clsx and sass dependency and add Layout component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,22 @@
+import Layout from "@/components/layout/layout";
+import Head from "next/head";
+import Link from "next/link";
+
 function About(){
     return (
         <>
+        <Layout>
+            <Head>
+                <title>About Page</title>
+            </Head>
         <h1>About Page</h1>
+        <div>
+            <Link href={"/"}> Back to Home</Link>
+        </div>
+        </Layout>
+        
         </>
     )
-
 }
 
 export default About;
