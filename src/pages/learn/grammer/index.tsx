ddcab2e08a81c82b04f0f38d0e034a8b1d6e3eb6
msucil <np.msushil@gmail.com>
2023-04-15 14:19:36 +0800

    remote html static code

--- src/pages/learn/grammer/index.tsx
+++ src/pages/learn/grammer/index.tsx
@@ -42,23 +42,6 @@ export default function GrammerHome({ articles }: InferGetStaticPropsType<typeof
                 // </a>
               )
               )}
-
-            <a href="#" className="list-group-item list-group-item-action">
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">List group item heading</h5>
-                <small className="text-body-secondary">3 days ago</small>
-              </div>
-              <p className="mb-1">Some placeholder content in a paragraph.</p>
-              <small className="text-body-secondary">And some muted small print.</small>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action">
-              <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">List group item heading</h5>
-                <small className="text-body-secondary">3 days ago</small>
-              </div>
-              <p className="mb-1">Some placeholder content in a paragraph.</p>
-              <small className="text-body-secondary">And some muted small print.</small>
-            </a>
           </div>
         </div>
       </div>
