51905da8bd1c700be3a3312db8ecc0f42021088c
msucil <np.msushil@gmail.com>
2023-04-24 01:44:48 +0800

    add next-pwa and configure it

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const path = require('path')
+
 
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 const nextConfig = {
   reactStrictMode: true,
   // sassOptions: {
@@ -13,4 +17,6 @@ const nextConfig = {
   // },
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true
+})
